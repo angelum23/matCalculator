08d15ee8b8334acb1fb2c82063b933e5cb8c998b
angelum23 <angelojosedarosa01@hotmail.com>
2022-05-30 13:03:09 -0300

    adicionados binds dos botões

--- src/pages/Calculadora.js
+++ src/pages/Calculadora.js
@@ -1,124 +1,149 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, Button, TouchableOpacity, BackHandler } from 'react-native';
 import { TextInput } from "react-native-gesture-handler";
+import { LineChart, Grid } from "react-native-svg-charts";
 
-var initialState = {
-    displayValue: '0',
-    operator: null,
-    firstValue: '',
-    secondValue: '',
-    nextValue: false
-}
-
-function executaClique(key){
-    switch (key) {
-        case 1:
-            displayValue = displayValue + '1'
-            break;
-    
-        default:
-            break;
-    }
-}
 
 export default function Calculadora(){
+    const [input, setInput] = useState(""); //valores bind
+    const [operacao, setOperacao] = useState("");
+
+    function executaClique(key){
+        let novoValor = input;
+        if (typeof key == "number") {
+            novoValor += key.toString();
+        }else{
+            novoValor += key;
+        }
+
+        setInput(novoValor)
+    }
+
+    function executaMaisMenos(key){
+        let novoValor = input;
+
+        novoValor += + key.toString();
+
+        setInput(novoValor)
+    }
+
+    function executaIgual(key){
+        let novoValor = input;
+
+        novoValor += + key.toString();
+
+        setInput(novoValor)
+    }
+
     return(
         <View style={styles.container}>
-            <View style={{flex:1}}></View>
+            <View style={{flex:1}}>
+                <LineChart
+                    style={{ height: 200 }}
+                    data={[50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80]}
+                    svg={{ stroke: 'rgb(134, 65, 244)' }}
+                    contentInset={{ top: 20, bottom: 20 }}
+                >
+                    <Grid />
+                </LineChart>
+            </View>
+
+        
 
             <View style={styles.ident1}>
                 <TextInput
                     style={styles.textInput}
                     editable
+                    value={input}
                     maxLength={30}
                 />
 
                 <View style={{flexDirection: "row"}}>
-                    <TouchableOpacity style={styles.button} onPress={() => this.executaClique(1)} >
+                    <TouchableOpacity style={styles.button}  onPress={() => executaClique(1)} >
                     <Text style={styles.buttonText}>1</Text>
                     </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.button} /* onPress={() => x} */>
+                    <TouchableOpacity style={styles.button} onPress={() => executaClique(2)}>
                     <Text style={styles.buttonText}>2</Text>
                     </TouchableOpacity>
                     
-                    <TouchableOpacity style={styles.button} /* onPress={() => x} */>
+                    <TouchableOpacity style={styles.button} onPress={() => executaClique(3)}>
                     <Text style={styles.buttonText}>3</Text>
                     </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.button} /* onPress={() => x} */>
+                    <TouchableOpacity style={styles.button} onPress={() => executaClique("/")}>
                     <Text style={styles.buttonText}>/</Text>
                     </TouchableOpacity>
                 </View>
 
                 <View style={{flexDirection: "row"}}>
-                    <TouchableOpacity style={styles.button} /* onPress={() => x} */>
+                    <TouchableOpacity style={styles.button} onPress={() => executaClique(4)}>
                     <Text style={styles.buttonText}>4</Text>
                     </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.button} /* onPress={() => x} */>
+                    <TouchableOpacity style={styles.button} onPress={() => executaClique(5)}>
                     <Text style={styles.buttonText}>5</Text>
                     </TouchableOpacity>
                     
-                    <TouchableOpacity style={styles.button} /* onPress={() => x} */>
+                    <TouchableOpacity style={styles.button} onPress={() => executaClique(6)}>
                     <Text style={styles.buttonText}>6</Text>
                     </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.button} /* onPress={() => x} */>
-                    <Text style={styles.buttonText}>x</Text>
+                    <TouchableOpacity style={styles.button} onPress={() => executaClique("*")}>
+                    <Text style={styles.buttonText}>*</Text>
                     </TouchableOpacity>
                 </View>
 
                 <View style={{flexDirection: "row"}}>
-                    <TouchableOpacity style={styles.button} /* onPress={() => x} */>
+                    <TouchableOpacity style={styles.button} onPress={() => executaClique(7)}>
                     <Text style={styles.buttonText}>7</Text>
                     </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.button} /* onPress={() => x} */>
+                    <TouchableOpacity style={styles.button} onPress={() => executaClique(8)}>
                     <Text style={styles.buttonText}>8</Text>
                     </TouchableOpacity>
                     
-                    <TouchableOpacity style={styles.button} /* onPress={() => x} */>
+                    <TouchableOpacity style={styles.button} onPress={() => executaClique(9)}>
                     <Text style={styles.buttonText}>9</Text>
                     </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.button} /* onPress={() => x} */>
+                    <TouchableOpacity style={styles.button} onPress={() => executaClique("-")}>
                     <Text style={styles.buttonText}>-</Text>
                     </TouchableOpacity>
                 </View>
 
                 <View style={{flexDirection: "row"}}>
-                    <TouchableOpacity style={styles.button} /* onPress={() => x} */>
+                    <TouchableOpacity style={styles.button} onPress={() => executaMaisMenos(1)}>
                     <Text style={styles.buttonText}>+/-</Text>
                     </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.button} /* onPress={() => x} */>
+                    <TouchableOpacity style={styles.button} onPress={() => executaClique(",")}>
                     <Text style={styles.buttonText}>,</Text>
                     </TouchableOpacity>
                     
-                    <TouchableOpacity style={styles.button} /* onPress={() => x} */>
+                    <TouchableOpacity style={styles.button} onPress={() => executaClique(0)}>
                     <Text style={styles.buttonText}>0</Text>
                     </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.button} /* onPress={() => x} */>
+                    <TouchableOpacity style={styles.button} onPress={() => executaClique("+")}>
                     <Text style={styles.buttonText}>+</Text>
                     </TouchableOpacity>
                 </View>
 
                 <View style={{flexDirection: "row"}}>
-                    <TouchableOpacity style={styles.button} /* onPress={() => x} */>
+                    <TouchableOpacity style={styles.button} onPress={() => executaClique("x")}>
                     <Text style={styles.buttonText}>x</Text>
                     </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.button} /* onPress={() => x} */>
+                    <TouchableOpacity style={styles.button} onPress={() => executaClique("(")}>
                     <Text style={styles.buttonText}>(</Text>
                     </TouchableOpacity>
                     
-                    <TouchableOpacity style={styles.button} /* onPress={() => x} */>
+                    <TouchableOpacity style={styles.button} onPress={() => executaClique(")")}>
                     <Text style={styles.buttonText}>)</Text>
                     </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.button} /* onPress={() => x} */>
+                    <TouchableOpacity style={styles.button} onPress={() => executaIgual()}>
                     <Text style={styles.buttonText}>=</Text>
                     </TouchableOpacity>
                 </View>
